5fe2aa27da14c987fed0831d348f2c345934b10b
callmewayne <591437639@qq.com>
2017-12-09 23:45:54 +0800

    产品详情 可以使用遍历轮播

--- myApp/src/app/carousel/carousel.component.ts
+++ myApp/src/app/carousel/carousel.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-
+  private Slides:Array<Slide>
   constructor() { }
 
   ngOnInit() {
+    this.Slides = [
+      new Slide(1),
+      new Slide(2),
+      new Slide(3),
+    ]
   }
 
+}
+export class Slide{
+  constructor(
+    public id:number,
+   
+  ){
+
+  }
 }
