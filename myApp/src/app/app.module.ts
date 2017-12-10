a6265c1724e1da999bc838300ea4f8ee9316c5b0
callmewayne <591437639@qq.com>
2017-12-10 23:21:31 +0800

    路由跳转，商品详情页

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from  '@angular/router';
 import { NgModule } from '@angular/core';
 
 
@@ -9,8 +10,17 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routeConfig:Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'product/:prodTitle',component:ProductDetailComponent
+  }
+]
 @NgModule({
   declarations: [//只能声明组件、指令�, NavbarComponent�, FooterComponent, SearchComponent, CarouselComponent, ProductComponent, StarsComponent管道
     AppComponent,
@@ -19,10 +29,13 @@ import { StarsComponent } from './stars/stars.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
+    ProductDetailComponent,
+    HomeComponent,
     StarsComponent
   ],
   imports: [
-    BrowserModule//需要正常运转还需要什么
+    BrowserModule,//需要正常运转还需要什么
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],//模块中提供了什么服务
   bootstrap: [AppComponent]//声明了模块的主组件
